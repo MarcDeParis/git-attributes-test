198409c193de61156cd3df673d06121823a14279
MarcDeParis <mnayman@oplead.com>
2021-09-06 08:12:54 +0200

    Add filter for js files

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-"// Sample JS File" 
+"// Sample JS File"
+
+const API_KEY = "a1693afec123";
+const makeAPICall = function(key) {
+    console.log("Makin API Call......");
+}
+
+makeAPICall(API_KEY);
